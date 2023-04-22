8f52ef89a802234b5476d24a39451d62c5b81362
Ricardo Robles <rroblesgo@gmail.com>
2023-04-22 19:32:23 +0200

    Pequeña modificación al Backend

--- drf_crud_api/settings.py
+++ drf_crud_api/settings.py
@@ -129,6 +129,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # cors authorization (lo modificamos al final)
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173"
 ]
 
 REST_FRAMEWORK = {
